29fa5c7cda4eaea645194551861757320a81fc9f
Nemixe <rafel.ami@gmail.com>
2025-01-23 09:41:34 +0700

    chore: fix validator variable

--- src/libs/env/index.ts
+++ src/libs/env/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  VITE_API_BASE_URL: z.string().url(),
   VITE_BASE_URL: z.string().url(),
   VITE_AUTH_FUSION_ID: z.string().min(1),
   VITE_AUTH_FUSION_TENANT_ID: z.string().min(1),
